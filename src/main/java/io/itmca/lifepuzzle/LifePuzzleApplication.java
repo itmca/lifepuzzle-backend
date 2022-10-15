c761ef528ed8712a3e0b2dff1b02c2d66d207afa
john <lord1229@gmail.com>
2022-10-15 21:14:06 +0900

    Initial Project Setting

---
+++ src/main/java/io/itmca/lifepuzzle/LifePuzzleApplication.java
@@ -0,0 +1,13 @@
+package io.itmca.lifepuzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifePuzzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifePuzzleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/itmca/lifepuzzle/LifePuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.itmca.lifepuzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifePuzzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
