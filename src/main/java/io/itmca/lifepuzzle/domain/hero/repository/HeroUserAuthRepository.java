d974bdd9c20c6a21bf2528793a3d5e30dc06ce4c
오솔미 <78997066+solmioh@users.noreply.github.com>
2024-03-05 21:33:38 +0900

    RELEASE 2024.03_2 (#49)
    * Create pull_request_template.md
    
    * feat: PR 템플릿 생성
    
    * [SHIP][LP-104] fix: 주인공 목록 조회 API 원복 및 새 API URL Path 생성 (#48)
    
    * [LP-104] fix: 주인공 목록 조회 원복 및 새 API URL 생성
    
    * [LP-104] fix: API URL  수정
    
    ---------
    
    Co-authored-by: ironprayer <40513725+ironprayer@users.noreply.github.com>

--- src/main/java/io/itmca/lifepuzzle/domain/hero/repository/HeroUserAuthRepository.java
+++ src/main/java/io/itmca/lifepuzzle/domain/hero/repository/HeroUserAuthRepository.java
@@ -1,6 +1,7 @@
 package io.itmca.lifepuzzle.domain.hero.repository;
 
 import io.itmca.lifepuzzle.domain.hero.entity.HeroUserAuth;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HeroUserAuthRepository extends JpaRepository<HeroUserAuth, Long> {
+
+  List<HeroUserAuth> findAllByUser_UserNo(Long userNo);
+
   @Query(value = "SELECT auth FROM HeroUserAuth auth "
       + "WHERE auth.user.userNo = :userNo AND auth.hero.heroNo = :heroNo")
   Optional<HeroUserAuth> findByUserNoAndHeroNo(@Param("userNo") Long userNo,
