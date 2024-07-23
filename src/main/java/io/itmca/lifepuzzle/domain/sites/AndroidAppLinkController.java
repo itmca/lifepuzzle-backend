61b1aac3ae8ad70b56cfdad75c466289270c51d4
john <34029413+border-line@users.noreply.github.com>
2024-07-23 21:36:20 +0900

    [LP-163] refactor: checkstyle에 맞추어 스타일 변경 (#83)
    Co-authored-by: jung.jooon <jung.jooon@yanolja.com>

--- src/main/java/io/itmca/lifepuzzle/domain/sites/AndroidAppLinkController.java
+++ src/main/java/io/itmca/lifepuzzle/domain/sites/AndroidAppLinkController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Android App Link에 필요한 json을 응답하기 위한 Controller
+ * Android App Link에 필요한 json을 응답하기 위한 Controller.
  * 해당 처리만을 위한 별도 서버를 둘 수 없어 해당 서버에서 처리하도록 함
  */
 @RestController
@@ -13,13 +13,15 @@ public class AndroidAppLinkController {
 
   @GetMapping("/.well-know/assetlinks.json")
   public String applinkAsset() {
-    return "[{\n" +
-        "  \"relation\": [\"delegate_permission/common.handle_all_urls\"],\n" +
-        "  \"target\": {\n" +
-        "    \"namespace\": \"android_app\",\n" +
-        "    \"package_name\": \"io.itmca.lifepuzzle\",\n" +
-        "    \"sha256_cert_fingerprints\": [\" FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C\"]\n" +
-        "  }\n" +
-        "}]";
+    return """
+            [{
+              "relation": ["delegate_permission/common.handle_all_urls"],
+              "target": {
+                "namespace": "android_app",
+                "package_name": "io.itmca.lifepuzzle",
+                "sha256_cert_fingerprints": [" FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C"]
+              }
+            }]
+        """;
   }
 }
