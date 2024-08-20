fc4b20f927a213bbd35a1f8cf594fbdb3c16f9ef
ironprayer <ironprayer1208@gmail.com>
2024-08-20 23:51:14 +0900

    fix: 21버전 배포를 위해 설정 수정

--- src/main/java/io/itmca/lifepuzzle/domain/HealthEndpoint.java
+++ src/main/java/io/itmca/lifepuzzle/domain/HealthEndpoint.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthEndpoint {
 
   @Operation(hidden = true)
-  @RequestMapping({"", "hc"})
+  @RequestMapping({"/", "hc"})
   public String healthCheck() {
     return "ok";
   }
