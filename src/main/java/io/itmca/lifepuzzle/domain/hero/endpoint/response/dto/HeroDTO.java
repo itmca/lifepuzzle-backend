77abc49b7aff1f97230d95aa5e01f716ed593969
john <34029413+border-line@users.noreply.github.com>
2025-01-16 22:09:39 +0900

    [LP-369] feat: 주인공 나이 만으로 변경 (#156)
    Co-authored-by: jung.jooon <jung.jooon@yanolja.com>

--- src/main/java/io/itmca/lifepuzzle/domain/hero/endpoint/response/dto/HeroDTO.java
+++ src/main/java/io/itmca/lifepuzzle/domain/hero/endpoint/response/dto/HeroDTO.java
@@ -5,6 +5,7 @@ import static io.itmca.lifepuzzle.global.constant.ServerConstant.S3_SERVER_HOST;
 
 import io.itmca.lifepuzzle.domain.hero.entity.Hero;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,7 +31,7 @@ public class HeroDTO {
       .name(hero.getName())
       .nickname(hero.getNickname())
       .birthdate(hero.getBirthday())
-      .age(LocalDate.now().getYear() - hero.getBirthday().getYear() + 1)
+      .age((int) ChronoUnit.YEARS.between(hero.getBirthday(), LocalDate.now()))
       .image(toFullImageUrl(hero.getHeroNo(), hero.getImage()))
       .build();
   }
