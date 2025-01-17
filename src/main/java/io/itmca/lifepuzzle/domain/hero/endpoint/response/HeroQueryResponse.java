9cafdb5ebcff1d2154ac9403e5067651c7cdceca
ironprayer <40513725+ironprayer@users.noreply.github.com>
2025-01-17 10:47:18 +0900

    feat: 주인공 권한 리스트 1.조회 유저, 2.이름으로 정렬 처리 (#160)

--- src/main/java/io/itmca/lifepuzzle/domain/hero/endpoint/response/HeroQueryResponse.java
+++ src/main/java/io/itmca/lifepuzzle/domain/hero/endpoint/response/HeroQueryResponse.java
@@ -4,6 +4,8 @@ import io.itmca.lifepuzzle.domain.hero.endpoint.response.dto.HeroQueryDTO;
 import io.itmca.lifepuzzle.domain.hero.endpoint.response.dto.HeroUserAuthQueryDTO;
 import io.itmca.lifepuzzle.domain.hero.entity.Hero;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,13 +19,23 @@ public class HeroQueryResponse {
   private int puzzleCnt;
 
   public static HeroQueryResponse from(Hero hero, Long userNo, int puzzleCnt) {
-    var heroUserAuthQueryDTOs = hero.getHeroUserAuths().stream()
+    var mainUserHeroAuthQueryDTO = hero.getHeroUserAuths().stream()
+        .filter(heroUserAuth -> heroUserAuth.getUser().getId().equals(userNo))
         .map(HeroUserAuthQueryDTO::from)
         .toList();
+    var otherUserHeroAuthQueryDTOs = hero.getHeroUserAuths().stream()
+        .filter(heroUserAuth -> !heroUserAuth.getUser().getId().equals(userNo))
+        .map(HeroUserAuthQueryDTO::from)
+        .sorted(Comparator.comparing(HeroUserAuthQueryDTO::getNickName))
+        .toList();
+
+    var heroAuthQueryDTOs = new ArrayList<HeroUserAuthQueryDTO>();
+    heroAuthQueryDTOs.addAll(mainUserHeroAuthQueryDTO);
+    heroAuthQueryDTOs.addAll(otherUserHeroAuthQueryDTOs);
 
     return HeroQueryResponse.builder()
         .hero(HeroQueryDTO.from(hero, userNo))
-        .users(heroUserAuthQueryDTOs)
+        .users(heroAuthQueryDTOs)
         .puzzleCnt(puzzleCnt)
         .build();
   }
