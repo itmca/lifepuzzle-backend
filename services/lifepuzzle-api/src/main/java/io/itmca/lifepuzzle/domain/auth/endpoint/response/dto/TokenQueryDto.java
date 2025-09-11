dd39e0511df0606eb89b09ab15162e1c89469dd2
border-line <lord1229@gmail.com>
2025-09-11 11:42:16 -0400

    chore: add deprecated to expireAt fields of token response

--- services/lifepuzzle-api/src/main/java/io/itmca/lifepuzzle/domain/auth/endpoint/response/dto/TokenQueryDto.java
+++ services/lifepuzzle-api/src/main/java/io/itmca/lifepuzzle/domain/auth/endpoint/response/dto/TokenQueryDto.java
@@ -4,8 +4,11 @@ import java.time.LocalDateTime;
 
 public record TokenQueryDto(
     String accessToken,
+    // TODO(joo-on): FE에서 만료 시간을 토큰 파싱해서 확인하도록 변경하였고 FE배포 이후 불필요한 만료시간 필드 제거
+    @Deprecated(forRemoval = true)
     LocalDateTime accessTokenExpireAt,
     String refreshToken,
+    @Deprecated(forRemoval = true)
     LocalDateTime refreshTokenExpireAt,
     String socialToken
 ) {}
